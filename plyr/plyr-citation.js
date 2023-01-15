fda7e74cbe2bb3c86f61410fc4eff93e3b5c6218
Ray000000 <94271266+Ray000000@users.noreply.github.com>
2023-01-15 10:39:54 +0800

    Update plyr-citation.js

--- plyr/plyr-citation.js
+++ plyr/plyr-citation.js
@@ -1,8 +1,1 @@
-var css = document.createElement('link');
-  css.rel = 'stylesheet';
-  css.href = 'https://cdn.plyr.io/3.7.3/plyr.css';
-  document.head.appendChild(css);
-
-function addScript(url){
-  document.write('<script src="https://cdn.plyr.io/3.7.3/plyr.js"></script>');
-}
+document.write('<script src="https://cdn.plyr.io/3.7.3/plyr.js"></script>');
