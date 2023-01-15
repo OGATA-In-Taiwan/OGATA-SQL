f9e69531b40b98cae6e1100f6013149cea5ce847
Ray000000 <94271266+Ray000000@users.noreply.github.com>
2023-01-15 09:45:28 +0800

    Update plyr-citation.js

--- plyr/plyr-citation.js
+++ plyr/plyr-citation.js
@@ -3,7 +3,4 @@ var css = document.createElement('link');
   css.href = 'https://cdn.plyr.io/3.7.3/plyr.css';
   document.head.appendChild(css);
 
-var  JSElement=document.createElement("script");
-  JSElement.setAttribute("type","text/javascript");
-  JSElement.setAttribute("src","https://cdn.plyr.io/3.7.3/plyr.js");
-  document.body.appendChild(JSElement);
+document.write('<script src="https://cdn.plyr.io/3.7.3/plyr.js"></script>'); //注意,此處須為相對於index.html的絕對路徑
