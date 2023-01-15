d859de3dac928590ebdaa4b45084dfadde4cd9b3
Ray000000 <94271266+Ray000000@users.noreply.github.com>
2023-01-15 09:40:09 +0800

    Create plyr-citation.js

---
+++ plyr/plyr-citation.js
@@ -0,0 +1,9 @@
+var css = document.createElement('link');
+  css.rel = 'stylesheet';
+  css.href = 'https://cdn.plyr.io/3.7.3/plyr.css';
+  document.head.appendChild(css);
+
+var  JSElement=document.createElement("script");
+  JSElement.setAttribute("type","text/javascript");
+  JSElement.setAttribute("src","https://cdn.plyr.io/3.7.3/plyr.js");
+  document.body.appendChild(JSElement);
