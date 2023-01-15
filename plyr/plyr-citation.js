23b42d80e94f904395f643d2e2cd7aa69b2b1f9a
Ray000000 <94271266+Ray000000@users.noreply.github.com>
2023-01-15 10:20:40 +0800

    Update plyr-citation.js

--- plyr/plyr-citation.js
+++ plyr/plyr-citation.js
@@ -3,4 +3,6 @@ var css = document.createElement('link');
   css.href = 'https://cdn.plyr.io/3.7.3/plyr.css';
   document.head.appendChild(css);
 
-document.write('<script src="https://cdn.plyr.io/3.7.3/plyr.js" type="text/javascript" charset="utf-8"></script>');
+function addScript(url){
+  document.write('<script src="https://cdn.plyr.io/3.7.3/plyr.js" type="text/javascript" charset="utf-8"></script>');
+}
